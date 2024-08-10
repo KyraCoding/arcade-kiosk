4b7ae756a5a58af1ccba1fdce549f256260ed66c
MrMe <76573108+MrMeCoding@users.noreply.github.com>
2024-08-10 00:04:04 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -1,10 +1,36 @@
 chrome.runtime.onInstalled.addListener(async () => {
-    console.log("Installed!")
-    chrome.alarms.create('checkAPI', { periodInMinutes: 0.5 });
+  console.log("Installed!")
+  chrome.alarms.create('checkAPI', { periodInMinutes: 5 });
 });
 chrome.alarms.onAlarm.addListener(async (alarm) => {
   if (alarm.name === "checkAPI") {
-      var data = await (await fetch("https://hackclub.com/api/arcade/shop/")).json()
-      console.log(data)
+    var response = await fetch("https://hackclub.com/api/arcade/shop/")
+    if (response.status != 200) {
+      console.log("Invalid API response! Quitting...")
+      return
+    }
+
+    var data = await response.json()
+    var old = chrome.storage.local.get("lastFetched").lastFetched
+    data.forEach((item) => {
+      
+    })
+
+
+    // Finish up
+    var parsed = new Map()
+    data.forEach((item) => {
+      const name = item.name
+      delete item.name
+      parsed.set(name, item)
+    })
+    console.log(parsed)
+    chrome.storage.local.set({ "lastFetched": JSON.stringify(parsed) })
+    chrome.storage.local.set({ "lastFetchedTime": Date.now() })
+  }
+});
+chrome.notifications.onClicked.addListener(notificationId => {
+  if (notificationId === 'shop-diff-notif') {
+    chrome.action.openPopup();
   }
 });
--- client.js
+++ client.js
@@ -1,13 +1,19 @@
-async function delay(ms) {
-    // return await for better async stack trace support in case of errors.
-    return await new Promise(resolve => setTimeout(resolve, ms));
+function formatPlural(num) {
+    if (num == 1) {
+        return ""
+    } else {
+        return "s"
+    }
 }
-async function checkForUpdates() {
-    console.log("Fetching!")
-    var response = await fetch("https://hackclub.com/api/arcade/shop/")
-    var data = await response.json()
-    //document.getElementById("data").innerHTML = JSON.stringify(data)
-    await delay(10000)
-    await checkForUpdates()
+async function updateLastChecked() {
+    chrome.storage.local.get("lastFetchedTime", function (data) {
+        console.log("Value is " + data.lastFetchedTime);
+        if (!data.lastFetchedTime) {
+            document.getElementById("lastChecked").innerHTML = `A check is scheduled to occur soon...`
+        } else {
+            document.getElementById("lastChecked").innerHTML = `Last checked ${Math.floor((Date.now()-data.lastFetchedTime)/(1000 * 60))} minute${formatPlural(data.lastFetchedTime)} ago...`
+        }
+
+    })
 }
-checkForUpdates()
+updateLastChecked()
